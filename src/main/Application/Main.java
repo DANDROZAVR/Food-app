8a92aac63e39d32de5aa18735c6b35f54888625c
DANDROZAVR <andrey8daletsky@gmail.com>
2021-04-04 21:56:25 +0300

    Small fixes

--- src/main/Application/Main.java
+++ src/main/Application/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -16,12 +19,14 @@ public class Main extends Application {
     }
     public static void main(String[] args) {
         try {
-            main.Data.Database.connect("jdbc:postgresql://localhost:5432/psimaj", "psimaj", "testpass");
+            main.Data.Database.connect("jdbc:postgresql://localhost:5432/test", "test", "testpass");
+            //int empty[] = {};
+            //System.out.println(main.Data.Query.getFullInformation("Products", empty));
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Error set connection to the database");
             System.exit(1);
         }
-        launch(args);
+        //launch(args);
     }
 }
