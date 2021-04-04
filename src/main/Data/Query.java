28a596499c1fdab944a75018f8f6edfd3650e554
DANDROZAVR <andrey8daletsky@gmail.com>
2021-04-04 21:26:41 +0300

    Added simple database query

---
+++ src/main/Data/Query.java
@@ -0,0 +1,20 @@
+package main.Data;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Query {
+    ArrayList<ArrayList<String>> getFullInformation(String fromTable, int[] order) throws SQLException {
+        StringBuilder query = new StringBuilder("SELECT * FROM " + fromTable);
+        if (order.length > 0) {
+            query.append("ORDER BY ");
+            for (int i = 0; i < order.length; ++i) {
+                if (i > 0)
+                    query.append(",");
+                query.append(order[i]);
+            }
+        }
+        query.append(";");
+        return Database.execute(query.toString());
+    }
+}
