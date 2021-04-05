29ba9dac10427e1887dbcae2d0bcfeeb9e97454f
daniil <yurshewich2002@gmail.com>
2021-04-05 07:01:57 +0300

    Absolutely new version

--- src/main/Application/Main.java
+++ src/main/Application/Main.java
@@ -11,12 +11,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 575, 275));
         primaryStage.show();
     }
     public static void main(String[] args) {
         try {
-            main.Data.Database.connect("jdbc:postgresql://localhost:5432/psimaj", "psimaj", "testpass");
+            main.Data.Database.connect("jdbc:postgresql://localhost:5432/test", "test", "testpass");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Error set connection to the database");
