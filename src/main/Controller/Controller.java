ea89f2c6b98624509e0ebdf37be0babe70259525
daniil <yurshewich2002@gmail.com>
2021-04-05 18:10:10 +0300

    Adding functional of the button products

--- src/main/Controller/Controller.java
+++ src/main/Controller/Controller.java
@@ -1,4 +1,47 @@
+// author: Daniil-Y
+
 package main.Controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button show_all_produkts;
+
+    @FXML
+    void initialize() {
+        assert show_all_produkts != null : "fx:id=\"show_all_produkts\" was not injected: check your FXML file 'sample.fxml'.";
+        show_all_produkts.setOnAction(event -> {
+            show_all_produkts.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/main/Application/forButtonProducts.fxml"));
+
+            try{
+                loader.load();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+    }
 }
