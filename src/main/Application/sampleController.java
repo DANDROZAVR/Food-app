60e062c300b7daa9a4c8783e5fe3206064e37d27
DANDROZAVR <andrey8daletsky@gmail.com>
2021-04-25 22:28:33 +0300

    first version of start buttons

--- src/main/Application/sampleController.java
+++ src/main/Application/sampleController.java
@@ -1,10 +1,9 @@
-// author: Daniil-Y
-
 package main.Application;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -24,14 +23,18 @@ public class sampleController {
 
     @FXML
     private Button show_all_receipts;
-
+    @FXML
+    void openSettings(ActionEvent event) {
+    }
+    @FXML
+    void goToHome(ActionEvent event) {
+    }
     @FXML
     void initialize() {
         assert show_all_products != null : "fx:id=\"show_all_products\" was not injected: check your FXML file 'sample.fxml'.";
         show_all_products.setOnAction(event -> {
             FXMLLoader loader = LoadXML.load("ForProducts.fxml");
             ForProductsController ctr = loader.getController();
-            //ctr.savePath("/main/Application/sample.fxml");
             Parent root = loader.getRoot();
             ((Stage) show_all_products.getScene().getWindow()).setScene(new Scene(root));
         });
