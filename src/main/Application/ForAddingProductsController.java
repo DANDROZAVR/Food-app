69d362417821ae42e3aa278ae305172210253073
AndreiMishchanka <andrew.mishchenko8@gmail.com>
2021-05-24 00:09:22 +0200

    change adding products

--- src/main/Application/ForAddingProductsController.java
+++ src/main/Application/ForAddingProductsController.java
@@ -2,6 +2,7 @@ package main.Application;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.beans.Observable;
@@ -16,8 +17,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import main.Data.Parser;
 import main.Data.Query;
 import main.Model.Products.Product;
 
@@ -55,17 +58,53 @@ public class ForAddingProductsController {
     private Button cancelButton;
     @FXML
     private Label error_out;
-
+    @FXML
+    private HBox clASSField;
     @FXML
     private void start_GetProductClass(){
-        ObservableList<String> GetProductClassList = FXCollections.observableArrayList("Species", "Solids", "Drinks");
-        GetProductClass.setValue("Species");
+        ObservableList<String> GetProductClassList = FXCollections.observableArrayList("Species", "Drinks", "Solids");
+        GetProductClass.setValue("");
         GetProductClass.setItems(GetProductClassList);
     }
 
     @FXML
     void initialize(){
+
+        // Parametrs
+        ChoiceBox<String> getTaste = new ChoiceBox<String>();
+        {
+            getTaste.setMaxHeight(35);
+            getTaste.setValue("");
+            getTaste.setItems(FXCollections.observableArrayList("Sweet", "Salty", "Bitter", "Sour"));
+        }
+
+        Label TextSugar = new Label("Sugar   ");
+
+        Label TextColour = new Label("   Colour   ");
+
+        TextField getSugar = new TextField();
+        TextField getColour = new TextField();
+        //
+
         start_GetProductClass();
+
+        GetProductClass.setOnAction(t -> {
+            clASSField.getChildren().clear();
+            getSugar.clear();
+            getColour.clear();
+            new ChoiceBox<String>();
+            {
+                getTaste.setMaxHeight(35);
+                getTaste.setValue("");
+                getTaste.setItems(FXCollections.observableArrayList("Sweet", "Salty", "Bitter", "Sour"));
+            }
+            if(GetProductClass.getValue() == "Species"){
+                clASSField.getChildren().add(getTaste);
+            }
+            if(GetProductClass.getValue() == "Drinks"){
+                clASSField.getChildren().addAll(TextSugar, getSugar, TextColour, getColour);
+            }
+        });
         AddProduct.setOnAction(event -> {
             int Id = 0;
             try{
@@ -74,21 +113,20 @@ public class ForAddingProductsController {
                 e.printStackTrace();
             }
             try {
-                String product_group = GetProductGroup.getText(), product_class = GetProductClass.getValue(), name = GetName.getText(), description = GetDescription.getText(), area = GetArea.getText();
-                int calories = Integer.parseInt(GetCalories.getText());
-                if(!Product.checkParameters(product_group, product_class, name, description, area, calories)){
-                    error_out.setTextFill(Color.web("#dd0e0e", 0.8));
-                    error_out.setText("ERROR");
-                    return;
-                }
-                boolean _try = Query.addNewProduct(Id, product_group, product_class, name, description, area, calories);
-                if(_try){
-                    error_out.setTextFill(Color.web("#16b221", 0.8));
-                    error_out.setText("OK");
-                }else{
-                    error_out.setTextFill(Color.web("#dd0e0e", 0.8));
-                    error_out.setText("ERROR");
-                }
+                String product_group = GetProductGroup.getText(), product_class = GetProductClass.getValue(), name = GetName.getText(), description = GetDescription.getText(), area = GetArea.getText(), calories = GetCalories.getText();
+                String sugar = getSugar.getText(), colour = getColour.getText(), taste = getTaste.getValue();
+                ArrayList< ArrayList<String> > forNewProduct = new ArrayList<>();
+                ArrayList<String> row = new ArrayList<String>();
+                row.add("id"); row.add("product_group"); row.add("product_class"); row.add("name"); row.add("description"); row.add("calories"); row.add("area"); row.add("sugar"); row.add("taste");
+                forNewProduct.add(new ArrayList<>(row));
+                row.clear();
+                row.add(Integer.toString(Id)); row.add(product_group); row.add(product_class); row.add(name); row.add(description); row.add(calories); row.add(area); row.add(sugar); row.add(taste);
+                forNewProduct.add(new ArrayList<>(row));
+                row.clear();
+                Product _new = Parser.getProductsFrom(forNewProduct).get(0);
+                Query.addNewProduct(_new);
+                error_out.setTextFill(Color.web("#16b221", 0.8));
+                error_out.setText("OK");
             }catch(Exception e){
                 e.printStackTrace();
                 error_out.setTextFill(Color.web("#dd0e0e", 0.8));
