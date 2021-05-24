ed83dde05b1bfa7c88ecb0d948645f6dbc637987
DANDROZAVR <andrey8daletsky@gmail.com>
2021-05-24 09:17:07 +0300

    big actualization

--- src/main/Model/Products/Product.java
+++ src/main/Model/Products/Product.java
@@ -5,14 +5,14 @@ import java.util.*;
 public abstract class Product {
     final private int callories;
     final private int id;
-    private final String productType;
-    protected String name, description;
-    String[] area, links;
-    protected Product(String name, int calories, String productType, int id) {
+    protected String name, description, productGroup;
+    String[] area, links, tags;
+
+    protected Product(String name, int calories, int id, String productGroup) {
         this.name = name;
         this.callories = calories;
-        this.productType = productType;
         this.id = id;
+        this.productGroup = productGroup;
     }
     public void setDescription(String description){ this.description = description; }
     public void setLinks(String[] links){ this.links = links; }
@@ -20,10 +20,10 @@ public abstract class Product {
     public int getCalories(){ return callories; }
     public String getName(){ return name; }
     public String getDescription(){ return description; }
-    public String getProductType(){ return productType; }
     public String[] getArea(){ return area; }
     public String[] getLinks(){ return links; }
-    public String getType() { return productType; }
+    public String getType() { return productGroup; }
+
     public static boolean checkParameters(String product_group, String product_class, String name, String description, String area, int calories){
         if(!(1 <= name.length() && name.length() <= 100)){
             return false;
