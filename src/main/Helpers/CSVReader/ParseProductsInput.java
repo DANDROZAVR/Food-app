fcea77e8ff4f9d83bb6d1d68972b4ff816cbc215
DANDROZAVR <andrey8daletsky@gmail.com>
2021-06-05 12:15:54 +0300

    not working csv parser

---
+++ src/main/Helpers/CSVReader/ParseProductsInput.java
@@ -0,0 +1,23 @@
+package main.Helpers.CSVReader;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ParseProductsInput {
+    public static void parse() {
+        try {
+            String project_path = new File(".").getCanonicalPath();
+            String file_path = "/src/resources/CSV/food2.csv";
+            ArrayList<String[]> table = CSVLoad.load(project_path + file_path);
+            table.get(0)[0] = "";
+            System.out.println(Arrays.toString(table.get(0)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        parse();
+    }
+}
